b1cc82b82e5c3539ca0327722a73e9317569a617
ZarriaFierra <113801420+ZarriaFierra@users.noreply.github.com>
2023-03-01 19:03:57 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,8 +32,8 @@ function setup() {
   World.add(world,player)
 
  // playerArcher = new ( 340, playerBase.position.y - 112, 120, 120);
- // playerArcher = new PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
-  playerArcher =  PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
+  playerArcher = new PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
+  //playerArcher =  PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
  // playerArcher = new PlayerArcher( );
 
 }
